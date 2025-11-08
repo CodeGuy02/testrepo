0d4b8a9cba87a2c65f7601dfeffb91b1b6f432b5
CodeGuy02 <86216164+CodeGuy02@users.noreply.github.com>
2025-11-07 22:20:40 -0500

    Create scope_lab.js

---
+++ scope_lab.js
@@ -0,0 +1,11 @@
+// Global scope:
+var globalVar = "I'm a global variable.";
+let globalLet = "I'm also a global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+// Block scope:
+var blockVar = "I'm a block-scoped var";
+var blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
